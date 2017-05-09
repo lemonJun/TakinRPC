8a1445de31d532288d0314ed73d95d77eb907977
lemonJun <506526593@qq.com>
2017-05-09 11:19:35 +0800

    去掉raft协议实现

--- takinrpc-raft/src/main/java/org/robotninjas/raft/LogListener.java
+++
@@ -1,7 +0,0 @@
-package org.robotninjas.raft;
-
-public interface LogListener {
-
-    void onCommit(byte[] entry);
-
-}
--- takinrpc-raft/src/main/java/org/robotninjas/raft/context/UpdateManagerFactory.java
+++
@@ -1,9 +0,0 @@
-package org.robotninjas.raft.context;
-
-import org.robotninjas.raft.ReplicaInfo;
-
-interface UpdateManagerFactory {
-
-    UpdateManager newManager(DefaultContext ctx, ReplicaInfo info, long nextIndex);
-
-}
